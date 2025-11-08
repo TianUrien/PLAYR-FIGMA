f9ba752d388c3dbeaa0300098db06005731d9973
TianUrien <tianurien@gmail.com>
2025-11-08 11:50:21 +0000

    Hide footer on immersive routes

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react'
+import { useLocation } from 'react-router-dom'
 import Footer from './Footer'
 import MobileBottomNav from './MobileBottomNav'
 
@@ -7,7 +8,14 @@ interface LayoutProps {
   className?: string
 }
 
+const HIDDEN_FOOTER_PREFIXES = ['/messages', '/onboarding', '/auth', '/dashboard']
+
 export default function Layout({ children, className = '' }: LayoutProps) {
+  const location = useLocation()
+  const shouldHideFooter = HIDDEN_FOOTER_PREFIXES.some(prefix =>
+    location.pathname.startsWith(prefix)
+  )
+
   return (
     <div className="flex flex-col min-h-screen">
       {/* Main content area - grows to push footer down */}
@@ -15,8 +23,8 @@ export default function Layout({ children, className = '' }: LayoutProps) {
         {children}
       </main>
 
-      {/* Footer - always at bottom */}
-      <Footer />
+      {/* Footer - hidden on immersive product surfaces */}
+      {!shouldHideFooter && <Footer />}
 
       {/* Mobile Bottom Navigation - Instagram style */}
       <MobileBottomNav />
